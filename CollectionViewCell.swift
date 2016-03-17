139dd9597152d7b4c1c2a57585ffcf608d43f069
padimi <bobbynwokonneya@gmail.com>
2016-03-17 09:24:57 +0000

    Added files via upload

---
+++ CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Padimi_1
+//
+//  Created by Bobby Nwokonneya  on 3/14/16.
+//  Copyright © 2016 Bobby Nwokonneya . All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
